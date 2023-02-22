15061a8b4e61f29f56da6d62fcf677e4f99d4d4d
finnp <contact@lippok.eu>
2023-02-22 18:59:20 +0100

    design

--- lib/views/splash_page.dart
+++ lib/views/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forum/palette.dart';
 import 'package:forum/services/remote_services.dart';
 import 'package:forum/views/home_page.dart';
 import 'package:forum/views/login_page.dart';
@@ -40,22 +41,31 @@ class _Splash_pageState extends State<Splash_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
       body: Center(
         child: _isLoading
-            ? CircularProgressIndicator()
-            : ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-                child: Text('Log In'),
+            ? const CircularProgressIndicator()
+            : Column(
+                children: <Widget>[
+                  const SizedBox(height: 150),
+                  Image.asset(
+                    'assets/images/ghse-banner.png',
+                    fit: BoxFit.contain,
+                    height: 150,
+                  ),
+                  const SizedBox(height: 200),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    child: const Text('Log In'),
+                  ),
+                ],
               ),
       ),
+      backgroundColor: Palette.BlueToLight[500],
     );
   }
 }
